3dd685a51deb766ad008ebc22da5edef845c6b96
Nate Bennett <nathanael.bennett@colorado.edu>
2019-10-15 12:26:39 -0600

    basic reqs met

--- Midterms/iOS midterm/iOS midterm 2 backup/ViewController.swift
+++ Midterms/iOS midterm/iOS midterm 2 backup/ViewController.swift
@@ -33,9 +33,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.resignFirstResponder()
         return true
     }
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        calculateStuff()
-    }
     @IBAction func buttonPushed(_ sender: UIButton) {
         calculateStuff()
     }
