8ccfd2fe199dcf4ba45f09e8250aecc88b6f1e2c
Nate Bennett <nathanael.bennett@colorado.edu>
2019-10-31 13:09:11 -0600

    fixed crashing error

--- Labs/lab6/app/src/main/java/com/example/birthday/MainActivity.java
+++ Labs/lab6/app/src/main/java/com/example/birthday/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.birthday;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -23,15 +24,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculateAge(View view){
-        ImageView cake = findViewById(R.id.cakeImage);
-        cake.setImageResource(R.drawable.maincake);
-        if(name.getText().toString() != "" && age.getText().toString() != ""){
+        if(!name.getText().toString().equals("") && !age.getText().toString().equals("")){
             String nameValue = name.getText().toString();
             int ageValue = Integer.parseInt(age.getText().toString());
             ageValue++;
 
             TextView results = findViewById(R.id.result);
             results.setText("Congrats " + nameValue + ", on your next birthday you will be " + ageValue + "!");
+            ImageView cake = findViewById(R.id.cakeImage);
+            cake.setImageResource(R.drawable.maincake);
+        } else{
+            TextView results = findViewById(R.id.result);
+            results.setText("Make sure you have entered a name and a birthday!");
         }
     }
 
