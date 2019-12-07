373942cb3ab486271eaf9e1f45bacc5bb0ce4b12
Nate Bennett <nathanael.bennett@colorado.edu>
2019-12-07 10:51:16 -0700

    done with project2

--- Projects/Project2/app/src/main/java/com/example/myboulderandroid/MainActivity.java
+++ Projects/Project2/app/src/main/java/com/example/myboulderandroid/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.myboulderandroid;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,6 +23,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
